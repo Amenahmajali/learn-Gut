0246d8064dd2d97f840681bff169bcdeb393ed21
AmenahAlmajli <majaliamenah@gmail.com>
2022-05-26 15:42:13 +0300

    add new object

--- app/src/main/java/com/example/mywwatherapp/ui/View.kt
+++ app/src/main/java/com/example/mywwatherapp/ui/View.kt
@@ -1,6 +1,37 @@
 package com.example.mywwatherapp.ui
 
+import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 object View {
+
+    const val DEBOUNCE_SEARCH_DELAY = 1000L
+
+
+    inline fun EditText.doOnDebounceQueryChange(
+        lifecycleOwner: LifecycleOwner,
+        crossinline action: (query: String) -> Unit
+    ) {
+        var searchFor = ""
+        doOnTextChanged { text, _, _, _ ->
+            val searchText = text.toString().trim()
+            if (searchText == searchFor)
+                return@doOnTextChanged
+
+            searchFor = searchText
+
+            lifecycleOwner.lifecycleScope.launch {
+                delay(DEBOUNCE_SEARCH_DELAY)
+                if (searchText != searchFor)
+                    return@launch
+                action(text.toString())
+            }
+        }
+    }
+
 }
